33d71f127b9523f73606fe30022860428dd1a996
Michael <michael.digiorgio70@qmail.cuny.edu>
2020-06-26 20:18:02 -0400

    navbar mobile

--- src/components/MainScroll/index.js
+++ src/components/MainScroll/index.js
@@ -1,4 +1,4 @@
-import React, {Suspense, lazy, useEffect} from 'react';
+import React, {Suspense, lazy, useState, useEffect} from 'react';
 // import SideNav from '../SideNav';
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import Projects from '../Projects';
@@ -12,7 +12,23 @@ const ArtGallery = lazy(() => import('../ArtGallery'))
 const ContactSection = lazy(() => import('../ContactSection'))
 
 function MainScroll() {
-
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    let dropdown = document.getElementById('dropdown')
+    let compStyles = window.getComputedStyle(dropdown);
+    let px = compStyles.getPropertyValue('top')
+    if (px === '0px') return
+    
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      // setScrollClass('navMobile')
+      document.getElementById('navMobile').style.top = '0'
+    } else {
+      // setScrollClass('navMobileHidden')
+      document.getElementById('navMobile').style.top = '-50px'
+    }
+    prevScrollpos = currentScrollPos;
+  }
   useEffect(() => {
     Events.scrollEvent.register('begin', function(to, element) {
       console.log("begin", arguments);
@@ -30,6 +46,14 @@ function MainScroll() {
     }
   })
 
+  function showDropdown() {
+    let dropdown = document.getElementById('dropdown')
+    let compStyles = window.getComputedStyle(dropdown);
+    let px = compStyles.getPropertyValue('top')
+    if (px === '-200px') document.getElementById('dropdown').style.top = '0px'
+    if (px === '0px') document.getElementById('dropdown').style.top = '-200px'
+  }
+
   const navTabReached = {
     backgroundColor: '#f7f3a9',
     cursor: 'pointer',
@@ -38,7 +62,18 @@ function MainScroll() {
 
   return (
     <>
-      {/* <SideNav /> */}
+      <div id='navMobile' onClick={() => showDropdown()}>
+        <div className='navDot'></div>
+        <div className='navDot'></div>
+        <div className='navDot'></div>
+      </div>
+      <div id='dropdown'>
+        <Link className='navTab' activeClass='active' to='projects' spy={true} smooth={true} offset={0} duration={1000}>Projects</Link>
+        <Link className='navTab' activeClass='active' to='photos' spy={true} smooth={true} offset={0} duration={1000}>Photo Journal</Link>
+        <Link className='navTab' activeClass='active' to='art' spy={true} smooth={true} offset={-100} duration={1000}>Art Gallery</Link>
+        <Link className='navTab' activeClass='active' to='design' spy={true} smooth={true} offset={-100} duration={1000}>Design Gallery</Link>
+        <Link className='navTab' activeClass='active' to="contact" spy={true} smooth={true} offset={0} duration={1000}>Contact</Link>
+      </div>
       <div id='sidebar'>
         <Link className='navTab' activeClass='active' to="contact" spy={true} smooth={true} offset={0} duration={1000}>Contact</Link>
         <Link className='navTab' activeClass='active' to='design' spy={true} smooth={true} offset={0} duration={1000}>Design Gallery</Link>
